a5392a86e5ef0ef5c7b3c45381e76516701f9ceb
radoykov <alex.radoykov@gmail.com>
2026-01-10 20:16:20 +0200

    Complete engine versin 2.0

--- engines/engines.ino
+++ engines/engines.ino
@@ -1,14 +1,19 @@
-//left and right engines
+//left engine
 int leftEnable = 2;
-int rightEnable = 3;
 int leftIn1 = 22;
 int leftIn2 = 23;
-int rightIn1 = 24;
-int rightIn2 = 25;
-
-//potentiometer
+int leftToggle = 24;
+int leftLed = 25;
 int leftThrottle = A0;
+
+//right engine
+int rightEnable = 3;
+int rightIn1 = 26;
+int rightIn2 = 27;
+int rightToggle = 28;
+int rightLed = 29;
 int rightThrottle = A1;
+
 int leftValue = 0;
 int rightValue = 0;
 
@@ -23,7 +28,7 @@ void setup() {
   pinMode(rightIn1, OUTPUT);
   pinMode(rightIn2, OUTPUT);
 
-  //set state
+  //set states
   digitalWrite(leftIn1, HIGH);
   digitalWrite(leftIn2, LOW);
   digitalWrite(rightIn1, LOW);
@@ -31,16 +36,28 @@ void setup() {
 }
 
 void loop() {
+  //left engine
   leftValue = analogRead(leftThrottle);
-  leftValue = map(leftValue, 0, 1023, 0, 255);
+  leftValue = map(leftValue, 0, 1023, 80, 255);  //80 is minimum thrust
+
+  if (digitalRead(leftToggle) == HIGH) {
+    digitalWrite(leftLed, HIGH);
+  } else {
+    leftValue = 0;
+    digitalWrite(leftLed, LOW);
+  }
   analogWrite(leftEnable, leftValue);
 
+  //right engine
   rightValue = analogRead(rightThrottle);
-  rightValue = map(rightValue, 0, 1023, 70, 255);
-  if (rightValue == 70) rightValue = 0;
-  analogWrite(rightEnable, rightValue);
+  rightValue = map(rightValue, 0, 1023, 80, 255);  //80 is minimum thrust
 
-  Serial.print(leftValue);
-  Serial.print(", ");
-  Serial.println(rightValue);
+  if (digitalRead(rightToggle) == HIGH) {
+    digitalWrite(rightLed, HIGH);
+  } else {
+    rightValue = 0;
+    digitalWrite(rightLed, LOW);
+  }
+  analogWrite(rightEnable, rightValue);
+  // Serial.println(rightValue);
 }
